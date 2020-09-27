189e0db4a3120195f1d87961621edce5b6dcbf34
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-09-27 18:15:53 -0300

    Teste: Cosplayer Choose

--- src/app/cosplayers/cosplayer-choose/cosplayer-choose.component.ts
+++ src/app/cosplayers/cosplayer-choose/cosplayer-choose.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Cosplayer } from './../../models/cosplayer.model';
+import { CosplayerService } from './../../services/cosplayer.service';
 
 @Component({
   selector: 'app-cosplayer-choose',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cosplayer-choose.component.scss']
 })
 export class CosplayerChooseComponent implements OnInit {
+  cosplayer: Cosplayer;
 
-  constructor() { }
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private cosplayerService: CosplayerService,
+  ) { }
 
   ngOnInit(): void {
+    this.subRouterParams();
+  }
+
+  private subRouterParams() {
+    this.activatedRouter.params.subscribe(params => {
+      const { idCosplay } = params;
+      this.loadCosplay(idCosplay);
+    });
+  }
+
+  loadCosplay(idCosplay: string) {
+    this.cosplayerService.getCosplayerById(idCosplay).subscribe(cosplayerInfo => {
+      this.cosplayer = cosplayerInfo;
+    });
   }
 
 }
