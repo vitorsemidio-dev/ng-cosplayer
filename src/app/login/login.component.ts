8c6f242b24d6d683862879ef7ae69bce0912ac41
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-09-27 21:38:34 -0300

    Feat: Layout formulário login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from './../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,17 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
   email: string;
   password: string;
 
   feedback: string;
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   handleLogin() {
     this.feedback = this.validateLogin(this.email, this.password);
+  }
+
+  handleSubmitForm() {
+    if (this.loginForm.valid) {
+      const resposne = this.authService.login(this.loginForm.value);
+      if (resposne) {
+        this.router.navigate(['/cosplayers']);
+      } else {
+        this.feedback = 'E-mail ou senha incorreto';
+      }
+    } else {
+      this.feedback = 'É necessário informar e-mail e senha';
+    }
   }
 
   private validateLogin(email: string, password: string) {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
-
 import { Subject } from 'rxjs';
 
+import { FakeDbService } from './../db/fake-db.service';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private isLogged = false;
   loginEmitter$ = new Subject<boolean>();
 
-  constructor() { }
+  constructor(private apiService: FakeDbService) {}
 
-  login() {
-    this.isLogged = true;
-    this.loginEmitter$.next(this.isLogged);
+  login({ email, password }: IRequest) {
+    const response = this.apiService.createSession({ email, password });
+
+    if (response.statusCode === 201) {
+      this.isLogged = true;
+      this.loginEmitter$.next(this.isLogged);
+      return true;
+    } else {
+      this.isLogged = false;
+      this.loginEmitter$.next(this.isLogged);
+      return false;
+    }
   }
 
   logout() {
