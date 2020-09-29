3fa207ac9053348d006ce99272067efba6b37d7a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-09-29 01:13:01 -0300

    Ajustes testes auth guard

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { CosplayersModule } from './../cosplayers/cosplayers.module';
 
 import { AuthGuard } from './auth.guard';
+import { AuthService } from './../services/auth.service';
 
-describe('AuthGuard', () => {
+interface IRequestMock {
+  email: string;
+  password: string;
+}
+class MockAuthService {
+  private isLogged = false;
+
+  login({ email, password }: IRequestMock) {
+    this.isLogged = true;
+  }
+
+  logout() {
+    this.isLogged = true;
+  }
+
+  getIsUserLogged() {
+    return this.isLogged;
+  }
+}
+
+fdescribe('AuthGuard', () => {
   let guard: AuthGuard;
+  let authService: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, CosplayersModule],
+      providers: [
+        AuthService,
+        { provide: AuthService, useClass: MockAuthService },
+      ],
+    });
     guard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should not be able to acess without login', () => {
+    expect(authService).toBeTruthy();
+    expect(guard.canActivate(null, null)).toBeFalse();
+  });
+
+  it('should be able to acess with login', () => {
+    expect(authService).toBeTruthy();
+    authService.login({ email: '', password: '' });
+    expect(guard.canActivate(null, null)).toBeTrue();
   });
 });
