06762b6832ed82083bf3cc3e4c4b1a9d6b5ee930
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-09-29 00:14:31 -0300

    Refatoração testes login

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
 
@@ -6,8 +7,17 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const correctEmail = 'user01@email.com';
+  const correctPassword = '123456';
+
+  const incorrectEmail = 'incorrect-email@email.com';
+  const incorrectPassword = 'incorrect-password';
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+      ],
       declarations: [LoginComponent],
     }).compileComponents();
   }));
@@ -23,26 +33,34 @@ describe('LoginComponent', () => {
   });
 
   it('should not login without provide email and password', () => {
-    component.handleLogin();
+    component.handleSubmitForm();
+
     expect(component.feedback).toContain('informar e-mail e senha');
   });
 
   it('should not be able to login with wrong email or passowrd', () => {
-    component.email = 'email@email.com';
-    component.password = '123456';
-    component.handleLogin();
-    expect(component.feedback).toContain('incorreto');
-
-    component.email = 'fake@email.com';
-    component.password = '1234567';
-    component.handleLogin();
+    component.loginForm.patchValue({
+      email: incorrectEmail,
+      password: incorrectPassword,
+    });
+    component.handleSubmitForm();
     expect(component.feedback).toContain('incorreto');
   });
 
   it('should be able show welcome mensage after login', () => {
-    component.email = 'fake@email.com';
-    component.password = '123456';
-    component.handleLogin();
-    expect(component.feedback).toContain('Bem vindo');
+    component.loginForm.patchValue({
+      email: correctEmail,
+      password: correctPassword,
+    });
+    component.handleSubmitForm();
+    expect(component.feedback).toBeUndefined();
+  });
+
+  xit('should validator email', () => {
+    // TODO
+  });
+
+  xit('should validator password minLength', () => {
+    // TODO
   });
 });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,17 +17,12 @@ export class LoginComponent implements OnInit {
       Validators.minLength(6),
     ]),
   });
-  email: string;
-  password: string;
 
   feedback: string;
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
-
-  handleLogin() {
-    this.feedback = this.validateLogin(this.email, this.password);
+  ngOnInit(): void {
   }
 
   handleSubmitForm() {
@@ -41,17 +36,5 @@ export class LoginComponent implements OnInit {
     } else {
       this.feedback = 'É necessário informar e-mail e senha';
     }
-  }
-
-  private validateLogin(email: string, password: string) {
-    if (!email || !password) {
-      return 'É necessário informar e-mail e senha';
-    }
-
-    if (email !== 'fake@email.com' || password !== '123456') {
-      return 'E-mail ou senha incorreto';
-    }
-
-    return 'Bem vindo, fake';
   }
 }
