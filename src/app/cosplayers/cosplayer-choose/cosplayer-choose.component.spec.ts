713d5f23be2ccca73d850171366ccdd2410893df
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-01 06:40:18 -0300

    Testes: Cosplayer Choose

--- src/app/cosplayers/cosplayer-choose/cosplayer-choose.component.spec.ts
+++ src/app/cosplayers/cosplayer-choose/cosplayer-choose.component.spec.ts
@@ -44,4 +44,84 @@ describe('CosplayerChooseComponent', () => {
     const h2: HTMLHeadingElement = elementComponent.querySelector('h2');
     expect(h2.innerText.trim()).toContain(component.cosplayer.name);
   });
+
+  it('should show cosplay name from a cosplayer', () => {
+    component.loadCosplay('1');
+    fixture.detectChanges();
+    const [
+      firstCosplayName,
+      secondCosplayName,
+    ] = component.cosplayer.cosplays.map((cosplay) => cosplay.name);
+    const li: HTMLLIElement = elementComponent.querySelector(
+      '.cosplay-list li'
+    );
+    expect(li.innerHTML.trim()).toContain(firstCosplayName, secondCosplayName);
+  });
+
+  it('should select a cosplay', () => {
+    component.loadCosplay('1');
+    fixture.detectChanges();
+    const [firstCosplay] = component.cosplayer.cosplays;
+    const { name: firstCosplayName } = firstCosplay;
+
+    component.chooseCosplay(firstCosplayName);
+    fixture.detectChanges();
+    const li: HTMLLIElement = elementComponent.querySelector('li.select');
+    expect(li.innerHTML.trim()).toContain(firstCosplayName);
+  });
+
+  it('should load Cosplay data without select', () => {
+    component.loadCosplay('1');
+    fixture.detectChanges();
+    const li: HTMLLIElement = elementComponent.querySelector('.select');
+    expect(li).toBeNull();
+  });
+
+  it('should change cosplay selected', () => {
+    component.loadCosplay('1');
+    fixture.detectChanges();
+    const [firstCosplay, secondCosplay] = component.cosplayer.cosplays;
+    const { name: firstCosplayName } = firstCosplay;
+    const { name: secondCosplayName } = secondCosplay;
+
+    const li: HTMLLIElement = elementComponent.querySelector('.select');
+    expect(li).toBeNull();
+
+    component.chooseCosplay(firstCosplayName);
+    fixture.detectChanges();
+    const liFirstCosplay: HTMLLIElement = elementComponent.querySelector(
+      '.select'
+    );
+    expect(liFirstCosplay.innerHTML.trim()).toContain(firstCosplayName);
+
+    component.chooseCosplay(secondCosplayName);
+    fixture.detectChanges();
+    const liSecondCosplay: HTMLLIElement = elementComponent.querySelector(
+      '.select'
+    );
+    expect(liSecondCosplay.innerHTML.trim()).toContain(secondCosplayName);
+    expect(liSecondCosplay.innerHTML.trim()).not.toContain(firstCosplayName);
+  });
+
+  it('should check the price with currency BRL', () => {
+    component.loadCosplay('1');
+    fixture.detectChanges();
+    const [firstCosplay] = component.cosplayer.cosplays;
+    const li: HTMLLIElement = elementComponent.querySelector(
+      '.cosplay-list li'
+    );
+    expect(li.innerHTML.trim()).toContain('R$', firstCosplay.price);
+  });
+
+  it('should rent a cosplayer', () => {
+    component.loadCosplay('1');
+    fixture.detectChanges();
+
+    const [firstCosplay] = component.cosplayer.cosplays;
+    component.chooseCosplay(firstCosplay.name);
+    fixture.detectChanges();
+
+    component.handleRent();
+    expect(component.idCosplayChoose).toBe(firstCosplay.name);
+  });
 });
